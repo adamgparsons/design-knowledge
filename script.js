ec5fc11f1c4913e6d7e1d09fb7e9d1c8b89cd973
adamgparsons <adamgeorgeparsons@gmail.com>
2019-11-12 17:19:45 +0000

    finished project

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
+const bodyTag = document.querySelector("body");
 const quoteTag = document.querySelector("h1");
+const authorTag = document.querySelector("p");
 const randomTag = document.querySelector("img");
 
-const data = ["this is quote 1", "this is quote 2", "this is quote 3"];
+// let data = [];
+
+// fetch from an api
+// then when there is a response
+// take that response and turn it into a JSON
+// then when that is done, make data = json
+// then get quote
+// fetch("https://api.superhi.com/api/test/quotes/")
+//   .then(response => response.json())
+//   .then(jsonData => {
+//     data = jsonData;
+//     // run getQuote
+//     getQuote();
+//   });
 
 const getQuote = function() {
-  // get a random quote
-  // put it in the site
+  fetch("https://api.superhi.com/api/test/quotes/random")
+    .then(response => response.json())
+    .then(jsonData => {
+      quoteTag.innerHTML = `&ldquo;${jsonData.quote}&rdquo;`;
+      authorTag.innerText = `-${jsonData.author}`;
+      if (jsonData.quote.length > 100) {
+        quoteTag.classList.add("long");
+      } else {
+        quoteTag.classList.remove("long");
+      }
+      bodyTag.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 90%)`;
+    });
 
-  const randomNumber = Math.floor(data.length * Math.random());
-  quoteTag.innerText = data[randomNumber];
+  // if (randomQuote.length > 0) {
+  //   console.log("hello");
+  // }
+
+  // if (data.length > 0) {
+  //   // get a random quote
+  //   const randomNumber = Math.floor(data.length * Math.random());
+  //   const randomQuote = data[randomNumber];
+
+  //   // put it in the site
+  //   quoteTag.innerText = randomQuote.quote;
+  //   authorTag.innerText = randomQuote.author;
+  // }
 };
 
-// run getQuote
 getQuote();
-
 // on click run getQuote
 randomTag.addEventListener("click", getQuote);
